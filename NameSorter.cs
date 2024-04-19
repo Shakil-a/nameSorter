937546c4d1f1a906189aea04579e3400d6effb4c
Shakil Ahmed <ahmed1shakil@gmail.com>
2024-04-19 14:38:40 +0100

    Implement feature:
    - Retrieve unsorted names from file
    - Sort alphabetically by last name
    -Srint sorted names to console to confirm it had worked.
    -Added the test file and final name sorted file

--- NameSorter.cs
+++ NameSorter.cs
@@ -8,8 +8,8 @@ public class NameSorter
 {
     public static void Main(string[] args)
     {
-        // checks if exactly one arguement has been provided (the unsorted names file), includes an error message and exits applications if there isnt only one and if there is
-        // then it stores it for later use
+        // checks if exactly one arguement has been provided (the unsorted names file), includes an error message and exits applications if there isnt only one 
+        // and if there is then it stores it for later use
         if (args.Length != 1)
         {
             Console.WriteLine("Error: please provide a file name in the command line");
@@ -17,7 +17,21 @@ public class NameSorter
         }
 
         string unsortedFileName = args[0];
-        string sortedFileName = "sorted-names-list.txt";
+        //string sortedFileName = "sorted-names-list.txt";
+
+        // retrive unsorted names from unsorted file and sort them alphabetically by last name, test if it had worked in console
+        List<string> unsortedNames = File.ReadAllLines(unsortedFileName).ToList();
+        List<string> sortedNames = unsortedNames.OrderBy(name => {
+            int lastSpacePosition = name.LastIndexOf(' ');
+            string lastName = name.Substring(lastSpacePosition + 1);
+            return lastName;
+        } ).ToList();
+
+          foreach (string name in sortedNames)
+        {
+            Console.WriteLine(name);
+        }
+
 
     }
 }
