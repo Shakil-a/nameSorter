09c96ca392d527f328d6f78efeba70f33bc6a44e
Shakil Ahmed <ahmed1shakil@gmail.com>
2024-04-20 15:48:42 +0100

    Refactored Main Method:
    -Extracted file reading, sorting, and printing logic into separate methods.
    -Tested solution against unsorted Names with the same LastName

--- NameSorter.cs
+++ NameSorter.cs
@@ -20,21 +20,45 @@ public class NameSorter
         string sortedFileName = "sorted-names-list.txt";
 
         // Read unsorted names from file and sort by last name alphabetically
-        List<string> unsortedNames = File.ReadAllLines(unsortedFileName).ToList();
-        List<string> sortedNames = unsortedNames.OrderBy(name => {
-            int lastSpacePosition = name.LastIndexOf(' ');
-            string lastName = name.Substring(lastSpacePosition + 1);
-            string OtherNames = name.Substring(0, lastSpacePosition);
-            return lastName + " " + OtherNames;
-        } ).ToList();
+        List<string> unsortedNames = ReadNamesFromFile(unsortedFileName);
+        List<string> sortedNames = SortNamesByLastName(unsortedNames);
 
         // Print sorted names to consolw and write to output file
-        foreach (string name in sortedNames)
+        PrintAndWriteSortedNames(sortedNames, sortedFileName);
+
+    }
+
+    private static List<string> ReadNamesFromFile(string fileName)
+    {
+        return File.ReadAllLines(fileName).ToList();
+    }
+
+    private static string GetLastName(string fullName)
+    {
+        int lastSpacePosition = fullName.LastIndexOf(' ');
+        string lastName = fullName.Substring(lastSpacePosition + 1);
+        return lastName;
+    }
+
+    private static string GetOtherNames(string fullName)
+    {
+        int lastSpacePosition = fullName.LastIndexOf(' ');
+        string otherNames = fullName.Substring(0, lastSpacePosition);
+        return otherNames;
+    }
+
+    private static List<string> SortNamesByLastName(List<string> names)
+    {
+        return names.OrderBy(name => GetLastName(name)).ThenBy(name => GetOtherNames(name)).ToList();
+    }
+
+    private static void PrintAndWriteSortedNames(List<string> names, string fileName)
+    {
+        foreach (string name in names)
         {
             Console.WriteLine(name);
         }
 
-        File.WriteAllLines(sortedFileName, sortedNames);
-
+        File.WriteAllLines(fileName, names);
     }
 }
